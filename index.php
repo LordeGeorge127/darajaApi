ff27e7563bf6ac73446f56c921da7f569a8cec48
LordeGeorge <george.ken.127@gmail.com>
2024-02-03 10:03:20 +0300

    authorzation

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php 
-echo "Daraja api";
+include 'keys.php';
+        $ch = curl_init('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Basic ' . base64_encode($consumerKey . ':' . $consumerSecret)
+        ]);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
+
+        $result = json_decode($response);
+        $access_token = $result->access_token;
+        curl_close($ch);
 ?>
